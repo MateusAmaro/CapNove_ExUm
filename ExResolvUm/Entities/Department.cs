4da75795cd63c17c5eabad34cc087262cf119094
Mateus Amaro <m4teusamaro@gmail.com>
2022-06-02 12:18:32 -0300

    Estudo sobre criacao de pastas e varias entidades

---
+++ ExResolvUm/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExResolvUm.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department (string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ ExResolvUm/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ExResolvUm.Entities.Enums;
+
+
+namespace ExResolvUm.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
